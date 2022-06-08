81da6a3713a230d1606c9326292d9d4bd87ae2ca
An Nguyen Q <an.nguyen@monstar-lab.com>
2022-06-08 15:32:41 +0700

    Setup Project

---
+++ FinalProject/Managers/PhotoManager.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoManager.swift
+//  FinalProject
+//
+//  Created by An Nguyen Q. VN.Danang on 08/06/2022.
+//
+
+import Foundation
+
+final class PhotoManager {
+    
+}
---
+++ FinalProject/Models/Scheme/Music.swift
@@ -0,0 +1,24 @@
+//
+//  Music.swift
+//  FinalProject
+//
+//  Created by An Nguyen Q. VN.Danang on 08/06/2022.
+//
+
+import Foundation
+
+final class Music {
+    var id: String
+    var artistName: String
+    var releaseDate: String
+    var name: String
+    var artworkUrl100: String
+    
+    init(json: JSObject) {
+        self.id = json["id"] as! String
+        self.artistName = json["artistName"] as! String
+        self.releaseDate = json["releaseDate"] as! String
+        self.name = json["name"] as! String
+        self.artworkUrl100 = json["artworkUrl100"] as! String
+    }
+}
