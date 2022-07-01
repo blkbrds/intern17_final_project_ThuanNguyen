19804034da90c5a54a645bf7800ea7bbbe5e4cc7
Thuan Nguyen <thuan.nguyen@monstar-lab.com>
2022-07-01 09:20:42 +0700

    Do Design UI PlayViewController

---
+++ FinalProject/Views/Detail/PlayMusic/PlayViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PlayViewController.swift
+//  MyMusic
+//
+//  Created by thuan.nguyen on 13/06/2022.
+//
+
+import UIKit
+
+final class PlayViewController: UIViewController {
+
+    // MARK: - IBOutlet
+    @IBOutlet private weak var imageView: UIImageView!
+
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigation()
+    }
+
+    // MARK: - private funtions
+    private func setupNavigation() {
+        title = "My Music"
+        let rightButton = UIBarButtonItem(image: UIImage(imageLiteralResourceName: "ic-reveal"), style: .plain, target: self, action: #selector(rightAction))
+        let downloadButton = UIBarButtonItem(image: UIImage(imageLiteralResourceName: "ic-heart"), style: .plain, target: self, action: #selector(downloadAction))
+        let closeButton = UIBarButtonItem(image: UIImage(imageLiteralResourceName: "ic-close"), style: .plain, target: self, action: #selector(closeAction))
+        self.navigationItem.rightBarButtonItems = [rightButton, downloadButton]
+        self.navigationItem.leftBarButtonItem = closeButton
+    }
+
+    // MARK: Actions
+    @objc func rightAction() {
+
+    }
+
+    @objc func downloadAction() {
+
+    }
+
+    @objc func closeAction() {
+
+    }
+}
---
+++ FinalProject/Views/Detail/PlayMusic/PlayViewControllerModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlayViewControllerModel.swift
+//  FinalProject
+//
+//  Created by thuan.nguyen on 30/06/2022.
+//
+
+import Foundation
